ea7cbab2c0c537d5a5b22583fd3f4ee94c583d15
Duc An <ducantran11@gmail.com>
2026-03-31 14:22:18 +0700

    iniit lab11

--- src/main/java/framework/base/DriverFactory.java
+++ src/main/java/framework/base/DriverFactory.java
@@ -1,25 +1,22 @@
-package framework.base;
+// Thêm 2 dòng import này ở đầu file
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class DriverFactory {
-
-    public static WebDriver createDriver(String browser) {
-        if (browser == null) {
-            browser = "chrome";
-        }
-        
-        switch (browser.toLowerCase()) {
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
-            case "chrome":
-            default:
-                WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
-        }
+public static WebDriver createDriver(String browser) {
+    if (browser == null) {
+        browser = "chrome";
+    }
+    
+    switch (browser.toLowerCase()) {
+        case "firefox":
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        case "edge":
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        case "chrome":
+        default:
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
     }
 }
